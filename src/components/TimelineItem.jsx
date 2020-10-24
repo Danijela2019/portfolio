0becebd7e3564783a22146b55ef9687c980feddc
Danijela2019 <arsic_danijela@yahoo.com>
2020-10-24 15:16:43 +0200

    added new projects page

---
+++ src/components/TimelineItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+ import  '../styles/TimelineItem.css'
+
+
+const TimelineItem = ({ data }) =>{
+    return  (
+        <main className="timeline_item">
+            <section className="timeline_item_content">
+                <time>{data.date}</time>
+                <p className="timeline_item_title">{data.text}</p>
+                <img className="timeline_item_img" src ={require (`../images/${data.img}`)}></img>
+                <div className="timeline_item_button-wrapper">
+                    <a href={data.link.url1} target="_blank" rel="noopener noreferrer">
+                            <button className="timeline_item_button">  {data.link.text1}</button>
+                    </a>
+                    <a href={data.link.url2} target="_blank" rel="noopener noreferrer">
+                            <button className="timeline_item_button">  {data.link.text2}</button>
+                    </a> 
+                </div>     
+                <span className="circle" />
+            </section>
+        </main>
+    );
+}
+
+export default TimelineItem;
---
+++ src/data.js
@@ -0,0 +1,45 @@
+const timelineData= [
+    {
+        text: 'The weather app',
+        date: 'June 10 2020',
+        img : 'projects/weatherapp.png',
+        link: {
+            url1:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text1: 'Demo',
+            url2:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text2: 'Code'
+        },
+    },
+    {
+        text: 'My movie app',
+        date: 'June 30 2020',
+        img : 'projects/mymovieapp.png',
+        link: {
+            url1:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text1: 'Demo',
+            url2:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text2: 'Code'
+        },
+    },
+    
+    
+]
+/*{
+    text: 'My second item on a timeline',
+    date: 'October 27 2020',
+    category: {
+        tag: 'medium',
+        color: '#018f69'
+    },
+    link: {
+        url:
+            'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+        text: 'Read more'
+    }
+}, */
+
+export default timelineData;
