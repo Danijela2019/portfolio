8b5efcc544cfc500862f59497352c91674e75216
Danijela2019 <arsic_danijela@yahoo.com>
2020-10-26 23:21:41 +0100

    Updated the project's timeline data

--- src/timeline_data.js
+++ src/timeline_data.js
@@ -25,6 +25,98 @@ const timelineData= [
             text2: 'Code'
         },
     },
+    {
+        text: 'Todo list JavaScript',
+        date: 'July 7th 2020',
+        img : 'projects/todo_javascript.png',
+        link: {
+            url1:
+                'https://todo-javascript.glitch.me/',
+            text1: 'Demo',
+            url2:
+                'https://github.com/Danijela2019/to-do-list-js',
+            text2: 'Code'
+        },
+    },
+    {
+        text: 'Todo list React',
+        date: 'July 14th 2020',
+        img : 'projects/todo_react.png',
+        link: {
+            url1:
+                'https://todo-react1.netlify.app/',
+            text1: 'Demo',
+            url2:
+                'https://github.com/Danijela2019/to-do-list-react',
+            text2: 'Code'
+        },
+    },
+    {
+        text: 'Food-Map (graduation group project)',
+        date: 'July 30th 2020',
+        img : 'projects/foodmap.png',
+        link: {
+            url1:
+                '',
+            text1: 'Video',
+            url2:
+                'https://github.com/Danijela2019/Food-Map',
+            text2: 'Code'
+        },
+    },
+    {
+        text: 'Gallery',
+        date: 'August 14th 2020',
+        img : 'projects/gallery.png',
+        link: {
+            url1:
+                'https://thegallery2020.herokuapp.com/',
+            text1: 'Demo',
+            url2:
+                'https://github.com/Danijela2019/Gallery',
+            text2: 'Code'
+        },
+    },
+    {
+        text: 'The meme maker',
+        date: 'September 1st 2020',
+        img : 'projects/mememaker.png',
+        link: {
+            url1:
+                'https://thememegenerator.netlify.app/',
+            text1: 'Demo',
+            url2:
+                'https://github.com/Danijela2019/The-meme-maker',
+            text2: 'Code'
+        },
+    },
+    {
+        text: 'Doggy-app',
+        date: 'September 29th 2020',
+        img : 'projects/doggyapp.png',
+        link: {
+            url1:
+                'https://github.com/Danijela2019/DoggyApp',
+            text1: 'Code F',
+            url2:
+                'https://github.com/Danijela2019/DoggyApp-backEnd',
+            text2: 'Code B'
+        },
+    },
+    {
+        text: 'Github-network',
+        date: 'September 15 2020',
+        img : 'projects/github.png',
+        link: {
+            url1:
+                'https://githubnet.netlify.app/',
+            text1: 'Demo',
+            url2:
+                'https://github.com/Danijela2019/Git-net',
+            text2: 'Code'
+        },
+    },
+    
     
     
 ]
