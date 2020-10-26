683ebbef84fbad56ce3c85e872546319a370d0da
Danijela2019 <arsic_danijela@yahoo.com>
2020-10-26 18:42:47 +0100

    Updated the footer and the navbar colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import '../styles/NavBar.css'
 const NavBar = () => { 
     return (
 
-<div className="navbar_container">NavBar
+<div className="navbar_container">
     <span className="navbar_item">Home</span>
     <span className="navbar_item">About</span>
     <span className="navbar_item">Experience</span>
