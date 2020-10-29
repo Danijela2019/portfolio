34dec605798e64d84c85eae171770eedb191b3d4
Danijela2019 <arsic_danijela@yahoo.com>
2020-10-29 10:13:55 +0100

    deployment

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <h1 className="notFound_header" style ={{marginTop:'4rem',fontSize:'3rem'}}>404 Page not found</h1>
+        <h1 className="notFound_header" style ={{marginTop:'5rem',fontSize:'3rem'}}>404 Page not found</h1>
     )
 }
 
