ca6f9f3632c849525a850c83f0bc5bbbde402e79
Danijela2019 <arsic_danijela@yahoo.com>
2020-10-31 13:20:00 +0100

    Some type O fixes in the timeline

--- src/data/timeline_data.js
+++ src/data/timeline_data.js
@@ -3,10 +3,10 @@ const timelineData = [
     title: 'The weather app',
     date: 'June 5th 2020',
     img: 'projects/weatherapp.png',
-    text: `The weatherapp was buld during my SALT course. It
+    text: `The weather app was built during my time at Salt. It
        was the first application that I build from scratch independently
-       using java script without my team. I chose server side rendering out of 
-       curiosity how it works and the express framework for the backend.`,
+       using JavaScript. I chose server side rendering out of 
+       curiosity, how it works and the Express framework for the backend.`,
       link: {
       url1:
                 'https://theweatherapp2020.herokuapp.com/nearby?lat=59.4252798&lon=17.8178287',
@@ -21,8 +21,8 @@ const timelineData = [
     date: 'June 25th 2020',
     img: 'projects/todo_javascript.png',
     text: `The todo list is another project created during my time at SALT
-       as a  weekend test assigment. The todo list
-        was created using only vanilla javaScript.`,
+       as a  weekend test assignment. The todo list
+        was created using only vanilla JavaScript.`,
     link: {
       url1:
                 'https://todo-javascript.glitch.me/',
@@ -34,8 +34,8 @@ const timelineData = [
   },
   {
     title: 'Todo list React',
-    text: `This app was a continuation of the todo list project as a weekend assigment,
-     but this time it was build using the React framework.`,
+    text: `This app was a continuation of the todo list project as a weekend assignment,
+     but this time it was built using the React framework.`,
     date: 'July 3th 2020',
     img: 'projects/todo_react.png',
     link: {
@@ -52,9 +52,9 @@ const timelineData = [
     date: 'July 15 2020',
     img: 'projects/mymovieapp.png',
     text: `The my movie app  was created as a one day project
-       during a Hack day at SALT. Since the hack day I developed it further. 
-       It is build using vanilla java script and  the express framework for the backend.
-        It is not completed yet. Next feature I will be working on is the user authentication.`,
+       during a Hack day at SALT.It is build using vanilla JavaScript and 
+        the Express framework for the backend.
+        I have develped it further, next feature I will be working on is the user authentication.`,
     link: {
       url1:
                 'https://mymovieapp2020.herokuapp.com/',
@@ -84,7 +84,7 @@ const timelineData = [
     title: 'Gallery',
     date: 'August 20th 2020',
     img: 'projects/gallery.png',
-    text: `This is my first independed project outside SALT,
+    text: `This is my first independent project outside SALT,
     the main purpose was to get familiar with the grid layout.`,
     link: {
       url1:
@@ -100,7 +100,7 @@ const timelineData = [
     date: 'September 1st 2020',
     img: 'projects/mememaker.png',
     text: `This is a mini project build using React class components 
-    without react hooks. I build this project to get more familar with
+    without react hooks. I build this project to get more familiar with
     class components in React.`,
     link: {
       url1:
@@ -115,9 +115,9 @@ const timelineData = [
     title: 'Doggy-app',
     date: 'September 29th 2020',
     img: 'projects/doggyapp.png',
-    text: ` This is a real mobile app build for a startup company, we build it with my team at SALT 
+    text: `This is a real mobile app build for a startup company, we build it with my team at SALT 
     as part of our post graduation program. We set up the project from scratch using expo platform, 
-    react native for the front and apollo for the backend and GraphQL for fething data.`,
+    react native for the front and Apollo for the back end and GraphQL for fetching data.`,
     link: {
       url1:
                 'https://github.com/Danijela2019/DoggyApp',
@@ -131,7 +131,7 @@ const timelineData = [
     title: 'Github-network',
     date: 'October 2 2020',
     img: 'projects/github.png',
-    text: ` This project was build as an idea how to use github's API.
+    text: `This project was built as an idea how to use github's API.
      I took the more modern approach and used react hooks and React Bootstrap for styling.`,
     link: {
       url1:
