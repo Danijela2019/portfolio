b73747eb4cf1fb528df29fb2f448ac01d4e58bfa
Danijela2019 <arsic_danijela@yahoo.com>
2020-11-04 16:09:46 +0100

    Updated the css for the mobile version

--- src/data/about_me_data.js
+++ src/data/about_me_data.js
@@ -1,20 +1,16 @@
 const aboutMeData = [
     {
         text: `I am an IT entusiast and life-long 
-         allways looking for some new chalenges in life.`
-    },
-    {
-        text: ` I love coding, creating new software 
+         allways looking for some new chalenges in life.
+          I love coding, creating new software 
         and see it coming to life is an amasing feeling.
         I have been through 2 bootcamps on my jurney of 
         becoming a software developer.`
     },
     {
         text: ` I gratuated from SALT recently and I am 
-        super existed for getting the chanse to do what I love to do.`
-    },
-    {
-        text: `I have a passion for teaching, I would love to become
+        super existed for getting the chanse to do what I love to do.
+        I have a passion for teaching, I would love to become
          a mentor one day and teach others what
         I know or maybe start writing blogs very soon.`
     },
