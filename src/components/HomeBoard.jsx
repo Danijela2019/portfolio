ba022885b9ab96a2a54c4cee51ef27dcb72868b2
Danijela2019 <arsic_danijela@yahoo.com>
2020-11-09 15:16:30 +0100

    Changed the color schema

---
+++ src/components/HomeBoard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../styles/HomeBoard.css';
+
+const HomeBoard = () => {
+    return (
+        <main className="homeboard_main" id="home">
+            <section className="background">
+                <article className="homeboard_transbox">
+                     <h1 className="homeboard_text">Hi! My name is Danijela Milenkovic.<br/>
+                     This is my coding jurney.</h1>
+                </article>
+            </section>
+        </main>
+    )
+
+}
+
+
+export default HomeBoard;
--- src/images/index.js
+++ src/images/index.js
@@ -15,10 +15,6 @@ export const sass = require('./logos/sass.png');
 export const styledComponents = require('./logos/styled_components.png');
 export const reactRouter = require('./logos/react_router.png');
 
-export const image1 = require('./slideImages/image1.jpg');
-export const image2 = require('./slideImages/image2.jpg');
-export const image3 = require('./slideImages/image3.jpg');
-
 export const doggyapp = require('./projects/doggyapp.png');
 export const foodmap = require('./projects/foodmap.png');
 export const gallery = require('./projects/gallery.png');
