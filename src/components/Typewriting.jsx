638910dcfb89fa35c5cbd09186069315fecf90cd
Danijela2019 <arsic_danijela@yahoo.com>
2020-11-10 09:16:13 +0100

    Updated the homepage

--- src/components/Typewriting.jsx
+++ src/components/Typewriting.jsx
@@ -13,13 +13,13 @@ const TypeWritingEfect =() => {
             onInit={(typewriter) => {
                 typewriter.typeString('Hi! My name is Danijela Milenkovic.')
                 .pauseFor(1500)
-                typewriter.typeString(`<h1>An IT entusiast.<h1/>`)
-                .pauseFor(2500)
-                .deleteChars(16)
-                typewriter.typeString('A life long learner.')
+                typewriter.typeString(`<h1>I'm an IT entusiast.<h1/>`)
                 .pauseFor(2500)
                 .deleteChars(20)
-                typewriter.typeString('A movie fan.')
+                typewriter.typeString(`I'm a life long learner.`)
+                .pauseFor(2500)
+                .deleteChars(24)
+                typewriter.typeString(`I'm a movie fan.`)
                 .pauseFor(2500)
                 .deleteChars(12)
                 .start();
