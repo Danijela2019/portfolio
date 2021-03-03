02702285f2c09e87921873007b510060e6a9562d
Danijela2019 <arsic_danijela@yahoo.com>
2021-03-03 19:23:12 +0100

    changed the background

--- src/pages/home/HomeBoard.jsx
+++ src/pages/home/HomeBoard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import '../../styles/HomeBoard.css';
-import TypeWritingEfect from './Typewriting';
+import '../../styles/HomeBoard.css'
+import Background from './Background'
 
 const HomeBoard = () => {
     return (
-        <main className="homeboard_main" id="home">
-            <TypeWritingEfect />
+        <main className='homeboard_main' >
+            <Background/>
         </main>
     );
 };
