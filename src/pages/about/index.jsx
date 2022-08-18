0b40434b0d96b049163ceffbbeafea9c45761b41
danijela.milenkovic <danijela.milenkovic@ddsthlm.se>
2022-08-18 21:59:57 +0200

    Update education page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -6,12 +6,11 @@ import '../../styles/about/Aboutme.css';
 const About = () => {
     return (
         <section className="aboutme_main" id="about">
-            <h1 className="aboutme_header">About me</h1>
-            <section className="aboutme_section">
+            <div>
                 <div className="aboutme_text">
                 <AboutMeText />
                 </div>
-            </section>
+            </div>
         </section>
     );
 };
